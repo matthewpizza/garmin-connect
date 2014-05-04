ae48c886c4dee5caeb96f0eaa7e94963c95bc31c
Matthew Spencer <hello@thisismatthew.com>
2014-05-04 14:35:56 -0400

    Get and set disk cache

--- lib/Garmin/API/Tools.php
+++ lib/Garmin/API/Tools.php
@@ -66,4 +66,119 @@ class Tools {
 		return $return;
 	}
 
+	/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+	/**
+	 * Set Cache Item
+	 *
+	 * @param string $key
+	 * @param array $data
+	 * @param string $group
+	 * @param int $duration
+	 *
+	 * @return bool $cache_set
+	 */
+	public static function cache_set($key, $data, $group = false, $duration = 3600) {
+		$cache_path = self::_cache_path();
+		$key = self::_sanitize_key( $key );
+		$filename = "{$key}";
+
+		if ( $group ) {
+			$group = self::_sanitize_key( $group );
+			$filename .= ".{$group}";
+		}
+		
+		$timestamp = time();
+
+		// serialize data with timestamp
+		$serialized_data = serialize( array('timestamp' => $timestamp, 'data' => $data) );
+
+		// write cache
+		$cache_set = file_put_contents( $cache_path . $filename, $serialized_data );
+
+		// file_put_contents() returns false on failure, number of bytes written on success
+		$cache_set = ( $cache_set === false ? $cache_set : true );
+
+		return $cache_set;
+	}
+
+	/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+	/**
+	 * Get Cache Item
+	 *
+	 * @param string $key
+	 * @param string $group
+	 * @param int $duration
+	 * 
+	 * @return bool|array
+	 */
+	public static function cache_get($key, $group = false, $duration = 3600) {
+		$cache_path = self::_cache_path();
+		$key = self::_sanitize_key( $key );
+		$filename = "{$key}";
+
+		if ( $group ) {
+			$group = self::_sanitize_key( $group );
+			$filename .= ".{$group}";
+		}
+
+		// get cache
+		$serialized_data = @file_get_contents( $cache_path . $filename );
+
+		// file_get_contents() returns false on failure
+		if ( $serialized_data === false ) return false;
+
+		$cache = unserialize( $serialized_data );
+		extract( $cache );
+
+		// return false if cache is expired
+		if ( time() > ($timestamp + $duration) ) return false;
+
+		return $data;
+	}
+
+	/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+	/**
+	 * Helper Function: Cache Path
+	 *
+	 * @return string $path
+	 */
+	private static function _cache_path() {
+		return dirname(dirname(dirname(__DIR__))) . '/cache/';
+	}
+
+	/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+	/**
+	 * Helper Function: Sanitize Cache Key
+	 * 
+	 * Clean up key name
+	 * Ex. users/john-doe => users.john-doe
+	 *
+	 * @param string $key
+	 * @return string $key
+	 */
+	private static function _sanitize_key( $key ) {
+		$search = array(
+			'/',
+			'&',
+			'=',
+			',',
+			'+'
+		);
+		$replace = array(
+			'.',
+			'.',
+			'_',
+			'-',
+			'-',
+		);
+
+		$key = strtolower( str_replace($search, $replace, $key) );
+
+		return $key;
+	}
+
 }
