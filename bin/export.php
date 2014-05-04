e1fe96700541b08824725d597e0d5e70cb0e5d8f
Matthew Spencer <hello@thisismatthew.com>
2014-05-04 19:54:21 -0400

    Run the exporter from the command line

---
+++ bin/export.php
@@ -0,0 +1,87 @@
+#!/usr/bin/php
+
+<?php
+
+/**
+ * Run the exporter
+ */
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/**
+ * Get Options
+ */
+
+$shortopts = 'u::';
+$shortopts .= 'p::';
+$shortopts .= 'o::';
+
+$longopts = array(
+	'username::',
+	'password::',
+	'output_path::',
+);
+
+$options = getopt($shortopts, $longopts);
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/**
+ * Set Variables
+ */
+$variable_to_set = array(
+	array(
+		'variable' => 'username',
+		'values' => array('u', 'username'),
+	),
+	array(
+		'variable' => 'password',
+		'values' => array('p', 'password'),
+	),
+	array(
+		'variable' => 'output_path',
+		'values' => array('o', 'output_path'),
+	),
+);
+
+foreach ( $variable_to_set as $option ) {
+	foreach ( $option['values'] as $key ) {
+
+		if ( isset($options[$key]) ) {
+			$$option['variable'] = $options[$key];
+			break;
+		}
+
+	}
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/**
+ * Make sure required options are set
+ */
+foreach (array('username', 'password', 'output_path') as $variable) {
+	if ( ! isset($$variable) ) {
+		die("Please set the {$variable} argument.\n");
+	}
+}
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/**
+ * Require the autoload
+ */
+$autoload = dirname(__DIR__) . '/vendor/autoload.php';
+
+if ( ! file_exists($autoload) ) {
+	die("Please generate the autoload by running `composer install`");
+}
+
+require_once $autoload;
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/**
+ * Run the exporter
+ */
+return new Garmin\API\Export($username, $password, $output_path);
