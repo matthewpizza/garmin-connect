8873792049d4532aedbc732ddbd6b4e382e24c9c
Matthew Spencer <hello@thisismatthew.com>
2014-12-06 21:10:10 -0500

    Go back to the old check (that didn’nt work) because auth is broken

--- src/Authenticate.php
+++ src/Authenticate.php
@@ -154,7 +154,7 @@ class Authenticate {
 
 		$response = Tools::curl($request, $options, $method);
 
-		if ( strpos($response['headers']['url'], 'sso.garmin.com/sso/login') === false ) {
+		if ( strpos($response['headers']['url'], 'connect.garmin.com') !== false ) {
 			return true;
 		}
 		else {
