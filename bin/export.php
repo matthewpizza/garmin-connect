d629af0568dbff672f8cb79bd2aabd698d00b3d6
Matthew Spencer <hello@thisismatthew.com>
2016-02-05 11:47:05 -0800

    Code smell.

--- bin/export.php
+++ bin/export.php
@@ -1,86 +1,58 @@
 #!/usr/bin/php
 <?php
 
-/**
- * Run the exporter
- */
-
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
-/**
- * Get Options
- */
-
+// Get Options
 $shortopts = 'e::';
 $shortopts .= 'p::';
 $shortopts .= 'o::';
 
-$longopts = array(
-	'email::',
-	'password::',
-	'output_path::',
-);
+$longopts = array( 'email::', 'password::', 'output_path::' );
 
-$options = getopt($shortopts, $longopts);
+$options = getopt( $shortopts, $longopts );
 
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
-/**
- * Set Variables
- */
+// Set Variables
 $variable_to_set = array(
 	array(
 		'variable' => 'email',
-		'values' => array('e', 'email'),
+		'values'   => array( 'e', 'email' ),
 	),
 	array(
 		'variable' => 'password',
-		'values' => array('p', 'password'),
+		'values'   => array( 'p', 'password' ),
 	),
 	array(
 		'variable' => 'output_path',
-		'values' => array('o', 'output_path'),
+		'values'   => array( 'o', 'output_path' ),
 	),
 );
 
 foreach ( $variable_to_set as $option ) {
 	foreach ( $option['values'] as $key ) {
-
-		if ( isset($options[$key]) ) {
-			$$option['variable'] = $options[$key];
-			break;
+		if ( ! isset( $options[ $key ] ) ) {
+			continue;
 		}
 
+		$$option['variable'] = $options[ $key ];
+		break;
 	}
 }
 
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
-/**
- * Make sure required options are set
- */
-foreach (array('email', 'password', 'output_path') as $variable) {
-	if ( ! isset($$variable) ) {
-		die( "Please set the {$variable} argument.\n" );
+// Make sure required options are set
+foreach ( array( 'email', 'password', 'output_path' ) as $variable ) {
+	if ( isset( $$variable ) ) {
+		continue;
 	}
+	die( "Please set the {$variable} argument.\n" );
 }
 
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+// Require the autoload
+$autoload = dirname( __DIR__ ) . '/vendor/autoload.php';
 
-/**
- * Require the autoload
- */
-$autoload = dirname(__DIR__) . '/vendor/autoload.php';
-
-if ( ! file_exists($autoload) ) {
+if ( ! file_exists( $autoload ) ) {
 	die( "Please generate the autoload by running `composer install`\n" );
 }
 
 require_once $autoload;
 
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
-/**
- * Run the exporter
- */
-return new MatthewSpencer\GarminConnect\Export($email, $password, $output_path);
+// Run the exporter
+return new MatthewSpencer\GarminConnect\Export( $email, $password, $output_path );
